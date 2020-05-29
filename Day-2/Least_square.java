68c49af9a57cf18be1a31fc281399811a1c4fec6
GK-42 <65946531+GK-42@users.noreply.github.com>
2020-05-29 23:21:10 +0530

    Update Least_square.java

--- Day-2/Least_square.java
+++ Day-2/Least_square.java
@@ -1,5 +1,57 @@
 package least_perfect_square;
 
-public class Least_square {
-
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Least_square
+{
+    public static double lcm (double a,double b)
+    {
+        double start;
+        double flag=0;
+        double i=1;
+        double x=1;
+        if (a<b)
+            start = b;
+        else
+            start = a;
+        while (flag==0)
+        {
+             x=a*i;
+            if (x % b == 0)
+            {
+                flag=1;
+            }
+            i++;
+        }
+        return x;
+    }
+    public static boolean perf (double a)
+    {
+        double sq = Math.sqrt (a);
+        return ((sq-Math.floor (sq))==0);
+    }
+    
+   public static void main(String[] args) 
+  {
+    double a=21;
+    double b=36;
+    double c=66;
+    double lcma,lcmf;
+    lcma = lcm (a,b);
+    lcmf = lcm (lcma,c);
+    double i=1;
+    double flag=1;
+    double temp;
+    while (flag==1)
+    {
+        temp =lcmf * i;
+        if (perf (temp))
+        {
+            System.out.println (temp);
+            flag=0;
+        }
+        i++;
+    }
+  }
 }
